34645361470c764e9b05da3d0a3bc038e239b032
ewertonurias <26447762+ewertonurias@users.noreply.github.com>
2025-01-07 14:04:02 -0400

    Add files via upload

---
+++ 1_add_contrib_nonfree.sh
@@ -0,0 +1,49 @@
+ #!/bin/bash
+
+# Verifica se o script está sendo executado como root
+if [ "$(id -u)" -ne 0 ]; then
+    printf "Este script precisa ser executado como root.\n"
+    exit 1
+fi
+
+# Caminho do arquivo sources.list
+SOURCES_LIST="/etc/apt/sources.list"
+
+# Gerando backup do sources.list original
+BACKUP_FILE=~/sources.list.bak
+printf "Fazendo backup do arquivo %s para %s...\n" "$SOURCES_LIST" "$BACKUP_FILE"
+cp "$SOURCES_LIST" "$BACKUP_FILE"
+
+# Deletando o arquivo original
+printf "Deletando o arquivo original...\n"
+rm "$SOURCES_LIST"
+
+# Gerando novo sources.list
+printf "Gerando novo sources.list com 'contrib' e 'non-free' em %s...\n" "$SOURCES_LIST"
+
+# Para Debian 12 (bookworm), ajuste conforme sua versão
+cat <<EOL >> $SOURCES_LIST
+
+deb http://deb.debian.org/debian/ bookworm main non-free-firmware contrib non-free
+deb-src http://deb.debian.org/debian/ bookworm main non-free-firmware contrib non-free
+
+deb http://security.debian.org/debian-security bookworm-security main non-free-firmware contrib non-free
+deb-src http://security.debian.org/debian-security bookworm-security main non-free-firmware contrib non-free
+
+# bookworm-updates, to get updates before a point release is made;
+# see https://www.debian.org/doc/manuals/debian-reference/ch02.en.html#_updates_and_backports
+deb http://deb.debian.org/debian/ bookworm-updates main non-free-firmware contrib non-free
+deb-src http://deb.debian.org/debian/ bookworm-updates main non-free-firmware contrib non-free
+
+# This system was installed using small removable media
+# (e.g. netinst, live or single CD). The matching "deb cdrom"
+# entries were disabled at the end of the installation process.
+# For information about how to configure apt package sources,
+# see the sources.list(5) manual.
+EOL
+
+printf "%s com 'contrib' e 'non-free' gerado com sucesso!\n" "$SOURCES_LIST"
+
+# Atualizando os pacotes
+printf "Atualizando pacotes...\n"
+apt update
---
+++ 2_add_fstab_entry.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Verifica se o script está sendo executado como root
+if [ "$(id -u)" -ne 0 ]; then
+    printf "Este script precisa ser executado como root.\n"
+    exit 1
+fi
+
+# Criando diretório games em /mnt
+printf "Criando o diretório games em /mnt...\n"
+sudo mkdir /mnt/games
+
+# Caminho do arquivo /etc/fstab
+FSTAB_FILE="/etc/fstab"
+
+# Entrada a ser adicionada
+NEW_ENTRY="UUID=f4525d07-661d-4c65-92b5-4dfaddf54c51 /mnt/games      ext4    defaults        0       0"
+
+# Faz um backup do /etc/fstab para a pasta pessoal do usuário
+BACKUP_FILE=~/fstab.bak
+printf "Fazendo backup do arquivo %s para %s...\n" "$FSTAB_FILE" "$BACKUP_FILE"
+cp "$FSTAB_FILE" "$BACKUP_FILE"
+
+# Verifica se a entrada já existe no arquivo
+if grep -Fxq "$NEW_ENTRY" "$FSTAB_FILE"; then
+    printf "A entrada já existe no arquivo %s. Nenhuma alteração foi feita.\n" "$FSTAB_FILE"
+else
+    # Adiciona a nova entrada ao final do arquivo
+    printf "Adicionando a nova entrada ao final do arquivo %s...\n" "$FSTAB_FILE"
+    printf "%s\n" "$NEW_ENTRY" >> "$FSTAB_FILE"
+    printf "Nova entrada adicionada com sucesso.\n"
+    printf "Reinicie o sistema para entrar em vigor.\n"
+fi
