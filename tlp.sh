3c832052a6d582b771e64e95f7dec5e23c103bbd
ewertonurias <26447762+ewertonurias@users.noreply.github.com>
2025-01-07 18:04:59 -0400

    Update tlp.sh

--- tlp.sh
+++ tlp.sh
@@ -15,3 +15,11 @@ sudo systemctl enable tlp.service
 # Verificando o estado do TLP
 printf "Verificando se o TLP está ativo...\n"
 sudo tlp-stat -s
+
+### Caso esteja longe de uma tomada e queira jogar,
+### desative temporariamente o TLP enquanto joga com o comando:
+### sudo tlp ac
+### Isso força o perfil de desempenho como se estivesse na tomada,
+### mesmo que esteja na bateria.
+### Para reativar o TLP normal depois:
+### sudo tlp start
